8d6e1c81c159459058b15b1687f8a3bf0c889bad
Sarah-0911 <sarah.tabet69@gmail.com>
2024-04-16 16:50:56 +0200

    ex 19 finished (without greenScreen method)

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -5,7 +5,7 @@ const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
 
 const getVideo = () => {
-  navigator.mediaDevices.getUserMedia({ video: false, audio: false })
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
     .then(localMediaStream => {
       console.log(localMediaStream);
       video.srcObject = localMediaStream;
@@ -24,6 +24,18 @@ const paintToCanvas = () => {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+
+    // take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+
+    // mess with them
+    // pixels = redEffect(pixels);
+    // pixels = rgbSplit(pixels);
+    // ctx.globalAlpha = 0.8;
+    ctx.filter = 'grayscale(60%)';
+
+    // put them back
+    ctx.putImageData(pixels, 0, 0);
   }, 16)
 };
 
@@ -34,7 +46,34 @@ const takePhoto = () => {
 
   //take the data out of the canvas
   const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src="${data}" alt="Handsome Woman" />`;
+  strip.insertBefore(link, strip.firstChild);
 };
+
+const redEffect = pixels => {
+  for(let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100; //r
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; //g
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //b
+  }
+  return pixels;
+}
+
+const rgbSplit = pixels => {
+  for(let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0] + 100; //r
+    pixels.data[i + 500] = pixels.data[i + 1] - 50; //g
+    pixels.data[i - 550] = pixels.data[i + 2] * 0.5; //b
+  }
+  return pixels;
+}
+
+// const greenScreen = pixels => {
+
+// }
 
 getVideo();
 
