28334c526b8cb3f9421d826e94cfdc5cb6c5fc5d
Sarah-0911 <sarah.tabet69@gmail.com>
2024-05-21 18:47:15 +0200

    ex 29 in progress

---
+++ 29 - Countdown Timer/scripts-START.js
@@ -0,0 +1,23 @@
+let countdown;
+
+const timer = seconds => {
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds)
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    displayTimeLeft(secondsLeft)
+  }, 1000);
+}
+
+
+const displayTimeLeft = seconds => {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  console.log({minutes, remainderSeconds});
+}
